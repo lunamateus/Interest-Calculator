7f94db183f52c2f3c1694533f6e79d564e4b0b35
lunamateus <mateusluna91@gmail.com>
2024-03-03 20:26:18 -0300

    Added dates to table and formmethod to post

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import {Investment} from './investment.js'
-import { drawChart } from './chart.js';
+import {drawChart} from './chart.js';
+import {getMonthYears} from './date.js';
 
-const calculateButton = document.getElementById('calculate');
 const currentYearSpan = document.getElementById("currentYear");
 const resultsDiv = document.getElementById("results");
 
@@ -14,7 +14,6 @@ currentYearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("calculator-form").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent default form submission
-
   //Validate input data
   const fields = ["principal", "monthlyInvestment", "increaseRate", "interest", "years"];
   let isValid = true;
@@ -43,34 +42,40 @@ document.getElementById("calculator-form").addEventListener("submit", function(e
   );
   investment.generateData();
 
-  const header = ["Month", "Invested", "Total Amount"];
+  const columns = ["Month", "Date", "Invested", "Total Amount"];
   const table = document.createElement("table");
   const thead = document.createElement("thead");
   const tbody = document.createElement("tbody");
   const headerRow = document.createElement("tr");
+  const dates = getMonthYears(investment.getTotalMonths() + 1);
   
   table.classList.add("table", "table-hover");
   thead.classList.add("table-dark");
   
-  headerRow.innerHTML = `
-    <th scope="col">${header[0]}</th>
-    <th scope="col">${header[1]}</th>
-    <th scope="col">${header[2]}</th>`;
+  for (let column in columns) {
+    const header = document.createElement("th");
+    header.scope = "col";
+    header.textContent = columns[column];
+    headerRow.appendChild(header);
+  }
   thead.appendChild(headerRow);
   
   for (let month of investment.getMonths()) {  
     // Create and populate row elements
     const row = document.createElement("tr");
     const monthCell = document.createElement("th");
+    const dateCell = document.createElement("td");
     const investedCell = document.createElement("td");
     const amountCell = document.createElement("td");
 
-    monthCell.textContent = month;
     monthCell.scope = "row";
+    monthCell.textContent = month;
+    dateCell.textContent = dates[month];
     investedCell.textContent = `$${investment.getInvestedAmount(month).toLocaleString()}`;
     amountCell.textContent = `$${investment.getAmount(month).toLocaleString()}`;
-  
+
     row.appendChild(monthCell);
+    row.appendChild(dateCell);
     row.appendChild(investedCell);
     row.appendChild(amountCell);
     tbody.appendChild(row);
