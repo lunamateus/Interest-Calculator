0b5f8ff9fe4540f71380a3bddc7e65b184a8eabc
lunamateus <mateusluna91@gmail.com>
2024-03-23 19:35:03 -0300

    Fixed meta tags changes and improved loading of icons

--- js/ui.js
+++ js/ui.js
@@ -31,7 +31,7 @@ function addIcon(name) {
   return img;
 }
 
-function setTextContent(elementType, attribute, data, opt = {tooltip: false, icon: false, description: false}) {
+function setTextContent(elementType, attribute, data, opt = {tooltip: false, icon: false, content: false}) {
   const elements = document.querySelectorAll(elementType);
 
   for (const element of elements) {
@@ -41,8 +41,8 @@ function setTextContent(elementType, attribute, data, opt = {tooltip: false, ico
     if (text) {
       if (opt.tooltip) {
         new bootstrap.Tooltip(element, {title:text});
-      } else if (opt.description) {
-        element.description = text;
+      } else if (opt.content) {
+        element.content = text;
       } else {
         element.textContent = text;
         if (opt.icon) {
@@ -83,7 +83,7 @@ function loadTexts(data, lang) {
   setTextContent('span', 'id', data.span);
   setTextContent('button', 'id', data.buttons);
   setTextContent('a', 'data-text', data.language, {icon: true});
-  setTextContent('meta', 'name', data.title, {description: true});
+  setTextContent('meta', 'name', data.title, {content: true});
   if (document.location.pathname.includes('faq.html')) {
     setTextContent('h3', 'data-text', data.links);
     createAccordion(data.faq);
